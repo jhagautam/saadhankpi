9c9cf5cd75053286f01aa0adb270aa6c2ab06e10
Gautam Jha <gautamrps32@gmail.com>
2021-01-09 16:22:19 +0530

    exit button on charts

--- mainapp/static/js/dash.js
+++ mainapp/static/js/dash.js
@@ -1,8 +1,6 @@
 function screen_resize() {
     var h = parseInt(window.innerHeight);
     var w = parseInt(window.innerWidth);
-    document.getElementById("myBox1").requestFullscreen().catch((e) => {
-        });
     if(w <= 500) {
       screen.orientation.lock("landscape-primary")
         }
@@ -12,6 +10,7 @@ function getFullscreenElement() {
 }
 
 function toggleFullscreen() {
+  document.getElementById("myBox1").requestFullscreen()
     screen_resize();
     var x = document.getElementById("normal_mode");
     var y=  document.getElementById("fullscreen_mode");
@@ -27,82 +26,140 @@ function exitfull(){
 }
 
 function toggleFullscreen2() {
-  if (getFullscreenElement()) {
-    document.exitFullscreen();
-  } else {
-    document.getElementById("myBox2").requestFullscreen().catch((e) => {
-      console.log('hi');
-      
-    });
-  }
+  document.getElementById("myBox2").requestFullscreen()
+    screen_resize();
+    var x = document.getElementById("normal_mode2");
+    var y=  document.getElementById("fullscreen_mode2");
+    x.style.display= "none";
+    y.style.display = "block";
 }
+function exitfull2(){
+  document.exitFullscreen();
+    var x = document.getElementById("normal_mode2");
+    var y=  document.getElementById("fullscreen_mode2");
+    y.style.display= "none";
+    x.style.display = "block";
+}
+
 function toggleFullscreen3() {
-  if (getFullscreenElement()) {
-    document.exitFullscreen();
-  } else {
-    document.getElementById("myBox3").requestFullscreen().catch((e) => {
-      console.log('hi');
-      
-    });
-  }
+  document.getElementById("myBox3").requestFullscreen()
+    screen_resize();
+    var x = document.getElementById("normal_mode3");
+    var y=  document.getElementById("fullscreen_mode3");
+    x.style.display= "none";
+    y.style.display = "block";
+}
+function exitfull3(){
+  document.exitFullscreen();
+    var x = document.getElementById("normal_mode3");
+    var y=  document.getElementById("fullscreen_mode3");
+    y.style.display= "none";
+    x.style.display = "block";
 }
 function toggleFullscreen4() {
-  if (getFullscreenElement()) {
+  document.getElementById("customer_card").requestFullscreen()
+    screen_resize();
+    var x = document.getElementById("normal_mode4");
+    var y=  document.getElementById("fullscreen_mode4");
+    var z= document.getElementById("customerbtn1");
+    var w= document.getElementById("vendorbtn1");
+    w.style.display= "none";
+    z.style.display= "none";
+    x.style.display= "none";
+    y.style.display = "block";
+}
+function exitfull4(){
     document.exitFullscreen();
-  } else {
-    document.getElementById("myBox4").requestFullscreen().catch((e) => {
-      console.log('hi');
-      
-    });
-  }
+    var x = document.getElementById("normal_mode4");
+    var y=  document.getElementById("fullscreen_mode4");
+    var z= document.getElementById("customerbtn1");
+    var w= document.getElementById("vendorbtn1");
+    w.style.display= "block";
+    z.style.display= "block";
+    y.style.display= "none";
+    x.style.display = "block";
 }
 function toggleFullscreen5() {
-  if (getFullscreenElement()) {
-    document.exitFullscreen();
-  } else {
-    document.getElementById("myBox5").requestFullscreen().catch((e) => {
-      console.log('hi');
-      
-    });
-  }
+  document.getElementById("vendor_card").requestFullscreen()
+    screen_resize();
+    var x = document.getElementById("normal_mode5");
+    var y=  document.getElementById("fullscreen_mode5");
+
+    var z= document.getElementById("customerbtn2");
+    var w= document.getElementById("vendorbtn2");
+    w.style.display= "none";
+    z.style.display= "none";
+    x.style.display= "none";
+    y.style.display = "block";
+}
+function exitfull5(){
+  document.exitFullscreen();
+    var x = document.getElementById("normal_mode5");
+    var y=  document.getElementById("fullscreen_mode5");
+    var z= document.getElementById("customerbtn2");
+    var w= document.getElementById("vendorbtn2");
+    w.style.display= "block";
+    z.style.display= "block";
+    y.style.display= "none";
+    x.style.display = "block";
 }
 function toggleFullscreen6() {
-  if (getFullscreenElement()) {
-    document.exitFullscreen();
-  } else {
-    document.getElementById("myBox6").requestFullscreen().catch((e) => {
-      console.log('hi');
-      
-    });
-  }
+  document.getElementById("myBox6").requestFullscreen()
+    screen_resize();
+    var x = document.getElementById("normal_mode6");
+    var y=  document.getElementById("fullscreen_mode6");
+    x.style.display= "none";
+    y.style.display = "block";
+}
+function exitfull6(){
+  document.exitFullscreen();
+    var x = document.getElementById("normal_mode6");
+    var y=  document.getElementById("fullscreen_mode6");
+    y.style.display= "none";
+    x.style.display = "block";
 }
 function toggleFullscreen7() {
-  if (getFullscreenElement()) {
-    document.exitFullscreen();
-  } else {
-    document.getElementById("myBox7").requestFullscreen().catch((e) => {
-      console.log('hi');
-      
-    });
-  }
+  document.getElementById("myBox7").requestFullscreen()
+    screen_resize();
+    var x = document.getElementById("normal_mode7");
+    var y=  document.getElementById("fullscreen_mode7");
+    x.style.display= "none";
+    y.style.display = "block";
+}
+function exitfull7(){
+  document.exitFullscreen();
+    var x = document.getElementById("normal_mode7");
+    var y=  document.getElementById("fullscreen_mode7");
+    y.style.display= "none";
+    x.style.display = "block";
 }
 function toggleFullscreen8() {
-  if (getFullscreenElement()) {
-    document.exitFullscreen();
-  } else {
-    document.getElementById("myBox8").requestFullscreen().catch((e) => {
-      console.log('hi');
-      
-    });
-  }
+  document.getElementById("myBox8").requestFullscreen()
+    screen_resize();
+    var x = document.getElementById("normal_mode8");
+    var y=  document.getElementById("fullscreen_mode8");
+    x.style.display= "none";
+    y.style.display = "block";
+}
+function exitfull8(){
+  document.exitFullscreen();
+    var x = document.getElementById("normal_mode8");
+    var y=  document.getElementById("fullscreen_mode8");
+    y.style.display= "none";
+    x.style.display = "block";
 }
 function toggleFullscreen9() {
-  if (getFullscreenElement()) {
-    document.exitFullscreen();
-  } else {
-    document.getElementById("myBox9").requestFullscreen().catch((e) => {
-      console.log('hi');
-      
-    });
-  }
+  document.getElementById("myBox9").requestFullscreen()
+    screen_resize();
+    var x = document.getElementById("normal_mode9");
+    var y=  document.getElementById("fullscreen_mode9");
+    x.style.display= "none";
+    y.style.display = "block";
+}
+function exitfull9(){
+  document.exitFullscreen();
+    var x = document.getElementById("normal_mode9");
+    var y=  document.getElementById("fullscreen_mode9");
+    y.style.display= "none";
+    x.style.display = "block";
 }
